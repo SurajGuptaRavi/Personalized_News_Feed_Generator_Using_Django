253a63b5f5df4671f45e8d2b801b5beed0129490
Suraj Rajesh Gupta <61044439+SurajGuptaRavi@users.noreply.github.com>
2021-04-17 20:47:30 +0530

    username and password removed

--- personalized_nfg/settings.py
+++ personalized_nfg/settings.py
@@ -152,8 +152,8 @@ CELERY_TASK_SERIALIZER = 'json'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'teamdwsuraj@gmail.com'
-EMAIL_HOST_PASSWORD = 'dandwsuraj@2021'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USER_SSL = False
 
